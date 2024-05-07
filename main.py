fb0a9d1527d9ea70191f2ab3e9b428e09dea43dd
Patent-Pending1 <164189564+Patent-Pending1@users.noreply.github.com>
2024-05-06 22:03:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import tkinter as tk
 from Risk import Generation
 from Risk import neighbors
 # generates a board form the risk.py
-n = 200
+n = 400
 board = Generation.finalGen(n)
 root = tk.Tk()
 root.title("risk")
 
 # this set the size of the square make it what ever
-square_size = 10
+square_size = 3
 
 # random tk stuff (did not steal from there webstie)
 canvas = tk.Canvas(root, width=len(board[0])*square_size, height=len(board)*square_size)
@@ -19,13 +19,17 @@ for y, row in enumerate(board):
     for x, value in enumerate(row):
         if value == " ":
             if neighbors(board,"water",x,y,n) == 1:
-                color = "#0ec7c1"
+                color = "lightYellow"
             elif neighbors(board,"water",x,y,n) == 2 or neighbors(board,"water",x,y,n) == 3:
                 color = "#0e80c7"
             else:
                 color = "blue"
         elif value == '#':
             color = '#013220'
+        elif value == '!':
+            color = 'white'
+        elif value == 'I':
+            color = 'lightBlue'
         else:
             color = 'green'
         canvas.create_rectangle(x*square_size, y*square_size, (x+1)*square_size, (y+1)*square_size, fill=color)
