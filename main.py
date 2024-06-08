3919a12f317d2ff5b5525a4fab535708efe984ef
Patent-Pending1 <164189564+Patent-Pending1@users.noreply.github.com>
2024-06-08 10:41:01 -0400

    TEMP

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+from random import choice
 import tkinter as tk
 from Risk import Generation
 from Risk import neighbors
@@ -17,8 +19,10 @@ canvas.pack()
 
 # goes through the board
 for y, row in enumerate(board):
-    for x, value in enumerate(row):
-        if value == " ":
+    for x, values in enumerate(row):
+        if values.count('I') == 1:
+            color = '#7390B5'
+        elif values.count('-') == 1:
             if neighbors(board,"water",x,y,n) == 1:
                 if y > (n*3//5)*(1/4) and y < (n*3//5)*(3/4):
                     color = "#F6F693"
@@ -27,19 +31,22 @@ for y, row in enumerate(board):
             elif neighbors(board,"water",x,y,n) == 2 or neighbors(board,"water",x,y,n) == 3:
                 color = "#0e80c7"
             else:
-                color = "#003186"
-        elif value == '#':
-            color = '#013220'
-        elif value == '!' or value == '!!':
+                hexValues = ['#003186','#1B0794','#001A82']
+                color = choice(hexValues)
+        elif values.count('!') == 1 or values.count('!!') == 1:
             color = '#FFFFFF'
-        elif value == 'I':
-            color = '#7390B5'
-        elif value == '&':
-            color = '#C2823A'
-        elif value == '@':
-            color = '#e3e100'
-        elif value == '0' or value == '%':
-            color = '#027D00'
+        elif values.count('@') == 1:
+            hexValues = ['#e3e100','#C9C800','#9C9B00','#BDBB1C','#FFFD00']
+            color = choice(hexValues)
+        elif values.count('&') == 1:
+            hexValues = ['#C2823A','#9E692C','#AD722F','#A16B2F','#805321']
+            color = choice(hexValues)
+        elif values.count('#') == 1:
+            hexValues = ['#013220','#014220','#013810']
+            color = choice(hexValues)
+        elif values.count('+') == 1 or values.count('%') == 1:
+            hexValues = ['#027D00','#028D00','#026D00','#025F00']
+            color = choice(hexValues)
         canvas.create_rectangle(x*square_size, y*square_size, (x+1)*square_size, (y+1)*square_size, fill=color)
 
 root.mainloop()
